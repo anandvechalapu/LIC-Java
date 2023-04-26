c6a68184da3aa5cb2fbe58209300a65bea7bb770
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-26 13:08:02 +0530

    Updated

--- LIC-Java/src/main/java/com/Sacral/ai/repository/.java
+++ LIC-Java/src/main/java/com/Sacral/ai/repository/.java
@@ -1,42 +1,57 @@
 package com.Sacral.ai.repository;
 
-import com.Sacral.ai.models.QuotationMemberNominee;
-import com.Sacral.ai.models.QuotationMemberNomineeApiResponseDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
+import com.Sacral.ai.entity.PolicyServiceRequestDto;
+import com.Sacral.ai.entity.CommonResponseDto;
+import com.Sacral.ai.entity.PolicyServiceDto;
+import com.Sacral.ai.exception.ApplicationException;
+import com.Sacral.ai.exception.RequiredFieldException;
+import com.Sacral.ai.exception.RequestValidationException;
+import com.Sacral.ai.service.mergerSaveServiceImpl;
+import com.Sacral.ai.service.policySrvSearchByIdServiceImpl;
 
 @Repository
-public class QuotationMemberNomineeRepository {
+public class PolicyServicingControllerRepository {
 
-    @Autowired
-    private EntityManager em;
-
-    private static final Logger logger = LoggerFactory.getLogger(QuotationMemberNomineeRepository.class);
-
-    @Transactional
-    public QuotationMemberNomineeApiResponseDto deleteQuotationMemberNominee(Long nomineeId) {
-        QuotationMemberNomineeApiResponseDto responseDto = new QuotationMemberNomineeApiResponseDto();
+    public CommonResponseDto<PolicyServiceDto> getPolicyServiceById(PolicyServiceRequestDto requestDto) {
+        CommonResponseDto<PolicyServiceDto> response = new CommonResponseDto<>();
         try {
-            logger.info("Started deleting nominee with id: {}", nomineeId);
-            QuotationMemberNominee nominee = em.find(QuotationMemberNominee.class, nomineeId);
-            if (nominee != null) {
-                em.remove(nominee);
-                responseDto.setTransactionStatus("success");
-                responseDto.setTransactionMessage("deleted");
+            validatePolicyServiceRequestDto(requestDto);
+            if (requestDto.getServiceType().equals("POLSRV_TYPE")) {
+                response.setData(null);
+            } else if (requestDto.getServiceType().equals("POLSRV_MEMBER_TRASFER_IN_OUT_TYPE")) {
+                response.setData(mergerSaveServiceImpl.getPolicyServiceById(requestDto.getId()));
             } else {
-                responseDto.setTransactionStatus("error");
-                responseDto.setTransactionMessage("fail");
+                response.setData(policySrvSearchByIdServiceImpl.getPolicyServiceById(requestDto.getId()));
             }
-            logger.info("Finished deleting nominee with id: {}", nomineeId);
-        } catch (Exception e) {
-            logger.error("Error deleting nominee with id: {}", nomineeId, e);
-            responseDto.setTransactionStatus("error");
-            responseDto.setTransactionMessage("fail");
+            response.setTransactionStatus("SUCCESS");
+            response.setTransactionMessage("Data retrieved successfully.");
+        } catch (ApplicationException ex) {
+            response.setTransactionStatus("ERROR");
+            response.setTransactionMessage(ex.getMessage());
+        } catch (RequiredFieldException ex) {
+            response.setTransactionStatus("ERROR");
+            response.setTransactionMessage(ex.getMessage());
+        } catch (RequestValidationException ex) {
+            response.setTransactionStatus("ERROR");
+            response.setTransactionMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setTransactionStatus("ERROR");
+            response.setTransactionMessage("An error occurred while retrieving the policy service details.");
         }
-        return responseDto;
+        return response;
+    }
+
+    private void validatePolicyServiceRequestDto(PolicyServiceRequestDto requestDto) throws RequiredFieldException, RequestValidationException {
+        if (requestDto == null) {
+            throw new RequiredFieldException("The request body must not be empty.");
+        }
+        if (requestDto.getId() == null || requestDto.getId().isEmpty()) {
+            throw new RequestValidationException("The policy service ID must not be empty.");
+        }
+        if (requestDto.getServiceType() == null || requestDto.getServiceType().isEmpty()) {
+            throw new RequestValidationException("The service type must not be empty.");
+        }
     }
 }
